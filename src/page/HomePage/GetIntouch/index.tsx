9bba4d521e26f9708cf308624eb7d4dfcffcfa62
Jacob Allen Valderama <vjacoballen2040@gmail.com>
2023-08-06 00:35:10 +0800

    Hello

--- src/page/HomePage/GetIntouch/index.tsx
+++ src/page/HomePage/GetIntouch/index.tsx
@@ -38,7 +38,7 @@ export const GetInTouch = () => {
 
 
     return (
-        <div ref={ref} className=' flex items-center bg-white' id="contact">
+        <div  className=' flex items-center bg-white' id="contact">
             <div className='w-full  my-10  flex justify-center ' >
                 <div className='flex flex-col bg-white p-10 lowercase w-full md:w-[60%] '>
                     <div className={twMerge('duration-1000 transition-all ', currrentView ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0')}>
@@ -47,14 +47,14 @@ export const GetInTouch = () => {
 
                     <div className={twMerge('text-[20px] text-[#39383D]','duration-1000 transition-all ', currrentView ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-full')}>if you have any questions or would like to work with me, please feel free to email me and say hello!</div>
                     <form ref={formRef} onSubmit={(e)=>sendEmail(e)}>
-                        <div className='flex flex-col gap-5 mt-10'>
+                        <div  className='flex flex-col gap-5 mt-10'>
                             <input id="email" required name='email' className={twMerge('  w-full p-4 border  rounded-lg focus:shadow-purple-900 focus:shadow outline-none','duration-1000 transition-all ', currrentView ? 'opacity-100' : 'opacity-0')} placeholder='Enter your Email Address'/>
                             <input id="name" required name='name' className={twMerge('w-full p-4 border  rounded-lg focus:shadow-purple-900 focus:shadow outline-none','duration-[1300ms] transition-all ', currrentView ? 'opacity-100' : 'opacity-0')} placeholder='Enter your Name'/>
-                            <input id="subject" required name='subject' className={twMerge('w-full p-4 border  rounded-lg focus:shadow-purple-900 focus:shadow outline-none','duration-[1600ms] transition-all ', currrentView ? 'opacity-100' : 'opacity-0')} placeholder='Enter Subject'/>
+                            <input  id="subject" required name='subject' className={twMerge('w-full p-4 border  rounded-lg focus:shadow-purple-900 focus:shadow outline-none','duration-[1600ms] transition-all ', currrentView ? 'opacity-100' : 'opacity-0')} placeholder='Enter Subject'/>
                             <textarea name='message' required rows={5} className={twMerge('resize-none border rounded-lg p-4 focus:shadow-purple-900 focus:shadow outline-none','duration-[1900ms] transition-all ', currrentView ? 'opacity-100' : 'opacity-0')} placeholder='Message... '/>
                             {isSending && <span className='text-green-600 flex gap-2 items-center'>Message Send <BiCheckCircle/></span>}
                             {isError && <span className='text-red-600 flex gap-2 items-center'>Error Sending Message <BsExclamationCircle/></span>}
-                            <div className={twMerge('w-full flex justify-center','duration-1000 transition-all ', currrentView ? 'opacity-100' : 'opacity-0')}>
+                            <div ref={ref} className={twMerge('w-full flex justify-center','duration-1000 transition-all ', currrentView ? 'opacity-100' : 'opacity-0')}>
                                 <button type="submit" disabled={ !!isSending} className={twMerge(' rounded-lg   p-3 w-full bg-gradient-to-t from-purple-900 to-blue-900 text-white ', isSending && 'cursor-not-allowed opacity-25')}>{'Send'}</button>
                             </div>
                         </div> 
