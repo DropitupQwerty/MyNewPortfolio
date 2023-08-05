6ef54389b43f17ec70aaa6435dcbed71c0d94c85
Jacob Allen Valderama <vjacoballen2040@gmail.com>
2023-08-05 12:01:23 +0800

    add

--- src/page/HomePage/GetIntouch/index.tsx
+++ src/page/HomePage/GetIntouch/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 // import { useOnScreen } from 'utilities/UseOnScreen'
 import emailJs from '@emailjs/browser'
+import { twMerge } from 'tailwind-merge'
+import { useOnScreen } from 'utilities/UseOnScreen'
 
 
 const Service_ID = 'service_3d1soey'
@@ -8,8 +10,8 @@ const emailTemplate = 'template_w9ia62s'
 const publicKey='_p02JMH-OM30WSzUh'
 
 export const GetInTouch = () => {
-    // const [data  , setData] = React.useState<emailType | null>(null)
     const ref = React.useRef<HTMLDivElement>(null)
+    const currrentView = useOnScreen(ref)
     const formRef =  React.useRef<HTMLFormElement | null>(null)
 
 
@@ -33,15 +35,18 @@ export const GetInTouch = () => {
         <div ref={ref} className=' flex items-center bg-white' id="contact">
             <div className='w-full  my-10  flex justify-center ' >
                 <div className='flex flex-col bg-white p-10 lowercase w-full md:w-[60%] leading-none'>
-                    <div className='text-[40px] lg:text-[70px] font-extrabold bg-clip-text text-transparent bg-gradient-to-t from-purple-900 to-blue-900'>Email me here.</div>
-                    <div className='text-[20px]'>Want to work with me? Message me now!</div>
+                    <div className={twMerge('duration-1000 transition-all ', currrentView ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0')}>
+                        <div className='text-[40px] lg:text-[70px] font-extrabold bg-clip-text text-transparent bg-gradient-to-t from-purple-900 to-blue-900'>Email me here.</div>
+                    </div>
+
+                    <div className={twMerge('text-[20px] text-[#39383D]','duration-1000 transition-all ', currrentView ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-full')}>Want to work with me? Message me now!</div>
                     <form ref={formRef} onSubmit={(e)=>sendEmail(e)}>
                         <div className='flex flex-col gap-5 mt-10'>
-                            <input id="email" name='email' className='w-full p-4 border  rounded-lg focus:shadow-purple-900 focus:shadow outline-none' placeholder='Enter your Email Address'/>
-                            <input id="name" name='name' className='w-full p-4 border  rounded-lg focus:shadow-purple-900 focus:shadow outline-none' placeholder='Enter your Name'/>
-                            <input id="subject" name='subject' className='w-full p-4 border  rounded-lg focus:shadow-purple-900 focus:shadow outline-none' placeholder='Enter Subject'/>
-                            <textarea name='message' rows={5} className='resize-none border rounded-lg p-4 focus:shadow-purple-900 focus:shadow outline-none' placeholder='Message... '/>
-                            <div className='w-full flex justify-center'>
+                            <input id="email"  name='email' className={twMerge('  w-full p-4 border  rounded-lg focus:shadow-purple-900 focus:shadow outline-none','duration-1000 transition-all ', currrentView ? 'opacity-100' : 'opacity-0')} placeholder='Enter your Email Address'/>
+                            <input id="name" name='name' className={twMerge('w-full p-4 border  rounded-lg focus:shadow-purple-900 focus:shadow outline-none','duration-[1300ms] transition-all ', currrentView ? 'opacity-100' : 'opacity-0')} placeholder='Enter your Name'/>
+                            <input id="subject" name='subject' className={twMerge('w-full p-4 border  rounded-lg focus:shadow-purple-900 focus:shadow outline-none','duration-[1600ms] transition-all ', currrentView ? 'opacity-100' : 'opacity-0')} placeholder='Enter Subject'/>
+                            <textarea name='message' rows={5} className={twMerge('resize-none border rounded-lg p-4 focus:shadow-purple-900 focus:shadow outline-none','duration-[1900ms] transition-all ', currrentView ? 'opacity-100' : 'opacity-0')} placeholder='Message... '/>
+                            <div className={twMerge('w-full flex justify-center','duration-1000 transition-all ', currrentView ? 'opacity-100' : 'opacity-0')}>
                                 <button type="submit" className='font-bold  rounded-lg  py-2 w-full bg-gradient-to-t from-purple-900 to-blue-900 text-white '>Submit</button>
                             </div>
                         </div> 
--- src/page/HomePage/MyContacts/index.tsx
+++ src/page/HomePage/MyContacts/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { BiSolidQuoteAltLeft, BiSolidQuoteAltRight } from 'react-icons/bi'
+import { twMerge } from 'tailwind-merge'
 import { MySocmed } from 'utilities/Social'
+import { useOnScreen } from 'utilities/UseOnScreen'
 
 
 
 
 
 export const MyContacts = () => {
+    const ref = React.useRef<HTMLDivElement>(null)
+    const currentView = useOnScreen(ref)
 
 
     return (
       
-        <div className=' bg-white flex flex-col items-center justify-evenly ' >   
+        <div ref={ref} className=' bg-white flex flex-col items-center justify-evenly ' >   
             <div className='py-[40px] px-[20px] font-Montserrat text-center  text-[20px] text-[#393E46] flex justify-center'>
                 <div className='flex '><BiSolidQuoteAltLeft/> Mistakes are the seeds from which trees of knowledge grow<BiSolidQuoteAltRight/></div>
             </div>       
@@ -21,7 +25,9 @@ export const MyContacts = () => {
                         {
                             MySocmed.map((coms, index)=> 
                                 <a href={coms.link} target='_blank' key={index} className='text-[#393E46]  text-[20px] hover:scale-105  hover:drop-shadow-xl ' rel="noreferrer">
-                                    {coms.icon}
+                                    <div className={twMerge('duration-1000 transition-all ', currentView? 'opacity-100 scale-100' : 'opacity-0 scale-75')} style={{transitionDuration : `${index + 1}s`}}>
+                                        {coms.icon}
+                                    </div>
                                 </a>)
                         }
                     </div>
