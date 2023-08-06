2d7b0ef4521da1c7c6e3e9aef5e98fce4c2968f5
Jacob Allen Valderama <vjacoballen2040@gmail.com>
2023-08-06 17:29:24 +0800

    git push

--- src/components/AppSideButtons/index.tsx
+++ src/components/AppSideButtons/index.tsx
@@ -13,8 +13,11 @@ export const AppSideButtons = () => {
     const percent = (pagePercent / pageHeight ) * 100
 
     return (
-        <div className='fixed bottom-0 z-[99999]  h-screen w-[250vh]  overflow-hidden ' > 
-            <div  className={'relative  h-[4px]  bg-purple-900   duration-1000 transition-all'}  style={{width:`${(100 - percent) }%`}}  >
+        <div className='z-[99999] h-full relative  border border-red-500' > 
+            <div className='fixed bottom-0 w-[120vw]'>
+
+                <div  className={'h-[4px]  bg-purple-900   duration-400 transition-all'}  style={{width:`${(100 - percent) }%`}}  >
+                </div>
             </div>
         </div>
     )
